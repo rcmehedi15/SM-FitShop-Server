5836d408e5a69485e376771c98fc02ef822aae5a
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-06-10 15:57:24 +0600

    classes save database

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const port = process.env.PORT || 5000
 
 // middleware
 const corsOptions = {
-  origin: '*',
-  credentials: true,
-  optionSuccessStatus: 200,
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
 }
 app.use(cors(corsOptions))
 app.use(express.json())
@@ -17,47 +17,55 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mehedi15.lrak9tg.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 })
 
 async function run() {
-  try {
-    const usersCollection = client.db('SM-Fit').collection('users')
+    try {
+        const usersCollection = client.db('SM-Fit').collection('users')
+        const classesCollection = client.db('SM-Fit').collection('classes')
 
-    // save user email and role
-    app.put('/users/:email', async (req, res) => {
-        const email = req.params.email
-        const user = req.body
-        const query = { email: email }
-        const options = { upsert: true }
-        const updateDoc = {
-          $set: user,
-        }
-        const result = await usersCollection.updateOne(query, updateDoc, options)
-        console.log(result)
-        res.send(result)
-      })
+        // save user email and role
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const query = { email: email }
+            const options = { upsert: true }
+            const updateDoc = {
+                $set: user,
+            }
+            const result = await usersCollection.updateOne(query, updateDoc, options)
+            console.log(result)
+            res.send(result)
+        })
+        // save instructor added class database
+        app.post('/classes', async (req, res) => {
+            const singleClass = req.body;
+            console.log(singleClass);
+            const result = await classesCollection.insertOne(singleClass);
+            res.send(result);
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 })
-    console.log(
-      'Pinged your deployment. You successfully connected to MongoDB!'
-    )
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db('admin').command({ ping: 1 })
+        console.log(
+            'Pinged your deployment. You successfully connected to MongoDB!'
+        )
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir)
 
 app.get('/', (req, res) => {
-  res.send('SM-Fit Server is running..')
+    res.send('SM-Fit Server is running..')
 })
 
 app.listen(port, () => {
-  console.log(`SM-Fit is running on port ${port}`)
+    console.log(`SM-Fit is running on port ${port}`)
 })
